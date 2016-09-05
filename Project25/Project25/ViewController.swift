ddc9c1eb20fe47e8d59fe534fd7793ee9bc1757f
Jeff Eng <jeff@jeffeng.me>
2016-09-05 16:01:20 -0700

    added collection view methods to specify number of items in section and cell for row at index path.

--- Project25/Project25/ViewController.swift
+++ Project25/Project25/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var images = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: #selector(importPicture))
         
     }
 
@@ -22,6 +27,23 @@ class ViewController: UIViewController {
         
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageView", forIndexPath: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
+    
+    func importPicture() {
+        print("The right bar button item is being clicked")
+    }
 
 }
 
